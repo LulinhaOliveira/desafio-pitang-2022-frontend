a66f56ead8d66297ce98b9c3e119b734ab525831
Lulinha Oliveira <lulinhaoliveirademelo@gmail.com>
2022-04-21 17:16:09 -0300

    Tela Todos Agendamentos, Uso da API

--- scheduling_frontend/src/pages/Scheduling/Scheduling.js
+++ scheduling_frontend/src/pages/Scheduling/Scheduling.js
@@ -1,5 +1,17 @@
+import { Container, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const Scheduling = () => {
-  return <div>Create</div>;
+  return (
+    <Container>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="name@example.com" />
+        </Form.Group>
+      </Form>
+    </Container>
+  );
 };
 
 export default Scheduling;
--- scheduling_frontend/src/pages/Scheduling/index.js
+++ scheduling_frontend/src/pages/Scheduling/index.js
@@ -1,5 +1,76 @@
+import { Table, Container, Button } from "@mantine/core";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
+import axios from "../../services/api";
+
 const Schedulings = () => {
-  return <div>Text</div>;
+  const [schedulings, setSchedulings] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/")
+      .then((response) => setSchedulings(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const onUptaded = (id, status) => {
+    axios
+      .patch(`/${id}`, { status: status })
+      .catch((error) => console.error(error));
+
+    setSchedulings(
+      schedulings.filter((scheduling) => {
+        scheduling.users.filter((user) => {
+          if (user.id === id) {
+            user.status = status;
+          }
+
+          return user;
+        });
+        return scheduling;
+      })
+    );
+  };
+
+  const rows = schedulings.map((scheduling, index) => (
+    <tr key={index}>
+      <td>{scheduling.date_time}</td>
+      {scheduling.users.map((user, index) => (
+        <td key={index}>
+          Name : {user.name} <br />
+          Birth Date : {user.birth_date} <br />
+          Status : {user.status} <br />
+          <Button
+            onClick={() => onUptaded(user.id, "attended")}
+            className="m-1"
+          >
+            Attended
+          </Button>
+          <Button
+            onClick={() => onUptaded(user.id, "not_attended")}
+            className="m-1"
+          >
+            Not Attended
+          </Button>
+        </td>
+      ))}
+    </tr>
+  ));
+
+  return (
+    <Container>
+      <Table horizontalSpacing="xs" highlightOnHover className="border">
+        <thead>
+          <tr>
+            <th>Date Time</th>
+            <th>User 1</th>
+            <th>User 2</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+    </Container>
+  );
 };
 
 export default Schedulings;
